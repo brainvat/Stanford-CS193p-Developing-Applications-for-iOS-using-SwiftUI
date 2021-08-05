5965b4691aeb0b5011ae083576383b0a61b730b1
Allen Hammock <allenhammock42@gmail.com>
2021-08-05 16:32:02 -0500

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Me Again on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
