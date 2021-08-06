87cde52cbcb39bf4a07e5021bd4a54e631c1ff71
Allen Hammock <allenhammock42@gmail.com>
2021-08-05 22:28:01 -0500

    complete Lecture 1

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack (content: {
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                .stroke(lineWidth: 3)
+                .padding()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Hello, CS103p!")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Text("Welcome")
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.blue)
+            }
+        })
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
