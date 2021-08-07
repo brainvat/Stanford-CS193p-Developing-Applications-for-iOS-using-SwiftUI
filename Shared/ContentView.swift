c412ac5c4f2b151a973b3587c08a4fa7563ef7a8
Allen Hammock <allenhammock42@gmail.com>
2021-08-07 18:07:42 -0500

    Assignment 1 - Task 5, display a random number of cards for each theme, at least 8

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
         "autos" : ["🛩", "🚗", "⛵️", "🚝", "🛻", "🚠", "🦼", "🛵", "🚛", "🚓", "🛳"]
     ]
     
-    @State var cardCount = 5
+    @State var cardCount = 8
     @State var theme = "autos"
     
     var body: some View {
@@ -38,18 +38,22 @@ struct ContentView: View {
                 Spacer()
                 ThemeButton(icon: "car.circle", label: "Autos") {
                     theme = "autos"
+                    cardCount = Int.random(in: 8...cards[theme]!.count)
                 }
                 Spacer()
                 ThemeButton(icon: "flag.circle", label: "Flags") {
                     theme = "flags"
+                    cardCount = Int.random(in: 8...cards[theme]!.count)
                 }
                 Spacer()
                 ThemeButton(icon: "person.circle", label: "People") {
                     theme = "people"
+                    cardCount = Int.random(in: 8...cards[theme]!.count)
                 }
                 Spacer()
                 ThemeButton(icon: "eye.circle", label: "Faces") {
                     theme = "faces"
+                    cardCount = Int.random(in: 8...cards[theme]!.count)
                 }
                 Spacer()
             }.padding(.horizontal)
