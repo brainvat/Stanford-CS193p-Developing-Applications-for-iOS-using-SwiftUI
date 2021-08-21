d3f53480f98408006dc9aa222e79ecdc79d1cd27
Allen Hammock <allenhammock42@gmail.com>
2021-08-21 12:09:04 -0700

    complete assignment 2, required task 2; remove theme chooser and title

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,115 +1,100 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by Allen Hammock on 8/5/21.
-//
+    //
+    //  ContentView.swift
+    //  Shared
+    //
+    //  Created by Allen Hammock on 8/5/21.
+    //
 
-import SwiftUI
+    import SwiftUI
 
-struct ContentView: View {
-    @ObservedObject var game: EmojiMemoryGame
-    
-    @State var cardCount = 8
-    
-    var body: some View {
-        VStack {
-            Text("Memorize!")
-                .font(.largeTitle)
-                .padding()
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
-                    ForEach(game.cards) { card in
-                        CardView(card)
-                            .aspectRatio(2/3, contentMode: .fit)
-                            .onTapGesture {
-                                game.choose(card)
-                            }
-                    }
-                }
-                .padding(.horizontal)
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            }
-            HStack {
-                ForEach(EmojiMemoryGame.themes.indices) {index in
-                    Spacer()
-                    ThemeButton(icon: EmojiMemoryGame.themes[index][2],
-                                label: EmojiMemoryGame.themes[index][1]) {
-                        game.reset(index)
-                    }
-                }
-                Spacer()
-            }.padding([.horizontal, .bottom])
-        }
-    }
-    
-    init(_ game: EmojiMemoryGame) {
-        self.game = game
-    }
-}
-
-struct ThemeButton: View {
-    var icon: String
-    var label: String
-    var action: () -> ()
+    struct ContentView: View {
+        @ObservedObject var game: EmojiMemoryGame
         
-    var body: some View {
-        Button {
-            action()
-        } label: {
-            VStack {
-                Image(systemName: icon)
-                    .font(.largeTitle)
-                    .frame(height: 50)
-                Text(label)
-                    .font(.caption)
+        @State var cardCount = 8
+        
+        var body: some View {
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                        ForEach(game.cards) { card in
+                            CardView(card)
+                                .aspectRatio(2/3, contentMode: .fit)
+                                .onTapGesture {
+                                    game.choose(card)
+                                }
+                        }
+                    }
+                    .padding(.horizontal)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
+        }
+        
+        init(_ game: EmojiMemoryGame) {
+            self.game = game
+        }
+    }
+
+    struct ThemeButton: View {
+        var icon: String
+        var label: String
+        var action: () -> ()
+            
+        var body: some View {
+            Button {
+                action()
+            } label: {
+                VStack {
+                    Image(systemName: icon)
+                        .font(.largeTitle)
+                        .frame(height: 50)
+                    Text(label)
+                        .font(.caption)
+                }
             }
         }
     }
-}
 
-struct CardView: View {
-    var card: MemoryGame<String>.Card
-    
-    var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20.0)
-            
-            if card.isFaceUp {
-                shape.fill().foregroundColor(.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content)
-                    .font(.system(size: 50))
-                    .foregroundColor(.black)
-                    .padding()
-            } else if card.isMatched {
-                shape.opacity(0.0)
-            } else {
-                shape.fill()
+    struct CardView: View {
+        var card: MemoryGame<String>.Card
+        
+        var body: some View {
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 20.0)
+                
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: 3)
+                    Text(card.content)
+                        .font(.system(size: 50))
+                        .foregroundColor(.black)
+                        .padding()
+                } else if card.isMatched {
+                    shape.opacity(0.0)
+                } else {
+                    shape.fill()
+                }
+                
             }
-            
+            .multilineTextAlignment(.center)
+
         }
-        .multilineTextAlignment(.center)
-
-    }
-    
-    init(_ card: MemoryGame<String>.Card) {
-        self.card = card
-    }
-}
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        let game = EmojiMemoryGame()
-        Group {
-            ContentView(game)
-                .preferredColorScheme(.light)
-                .previewDisplayName("Light Mode")
-            ContentView(game)
-                .preferredColorScheme(.dark)
-                .previewDisplayName("Dark Mode")
+        
+        init(_ card: MemoryGame<String>.Card) {
+            self.card = card
         }
     }
-}
+
+
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            let game = EmojiMemoryGame()
+            Group {
+                ContentView(game)
+                    .preferredColorScheme(.light)
+                    .previewDisplayName("Light Mode")
+                ContentView(game)
+                    .preferredColorScheme(.dark)
+                    .previewDisplayName("Dark Mode")
+            }
+        }
+    }
