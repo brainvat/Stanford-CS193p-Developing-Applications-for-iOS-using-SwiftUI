500cd085365e0166a3a9b0066e4b348ac1ad2962
Allen Hammock <allenhammock42@gmail.com>
2021-09-06 17:01:37 -0700

    implement Pie shape according to demo shown in Lecture 6; TODO: figure out why radius needs to be scaled 60%

--- Shared/EmojiMemoryGameView.swift
+++ Shared/EmojiMemoryGameView.swift
@@ -22,7 +22,12 @@
                 .padding()
                 
                 AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-                    cardView(for: card)
+                    CardView(card)
+                        .padding(2)
+                        .foregroundColor(card.color)
+                        .onTapGesture {
+                            game.choose(card)
+                        }
                 }
                 
                 HStack {
@@ -44,20 +49,6 @@
             self.game = game
         }
         
-        @ViewBuilder
-        private func cardView(for card: EmojiMemoryGame.Card) -> some View {
-            if (card.isMatched && !card.isFaceUp) {
-                Rectangle().opacity(0.0)
-            } else {
-                CardView(card)
-                    .padding(2)
-                    .foregroundColor(card.color)
-                    .onTapGesture {
-                        game.choose(card)
-                    }
-            }
-        }
-        
         private struct GridDrawingConstants {
             static let gridItemSize: CGFloat = 60
         }
@@ -67,17 +58,19 @@
         private var card: EmojiMemoryGame.Card
         
         var body: some View {
-            GeometryReader(content: { geometry in
+            GeometryReader { geometry in
                 ZStack {
                     let shape = RoundedRectangle(cornerRadius: CardDrawingConstants.cornerRadius)
                     
                     if card.isFaceUp {
                         shape.fill().foregroundColor(.white)
                         shape.strokeBorder(lineWidth: CardDrawingConstants.lineWidth)
+                        Pie(startAngle: Angle(degrees: 0-90.0), endAngle: Angle(degrees: 110.0-90.0))
+                            .padding(CardDrawingConstants.piePadding)
+                            .opacity(CardDrawingConstants.pieOpacity)
                         Text(card.content)
                             .font(font(in: geometry.size))
                             .foregroundColor(.black)
-                            .padding()
                     } else if card.isMatched {
                         shape.opacity(0.0)
                     } else {
@@ -86,7 +79,7 @@
                     
                 }
                 .multilineTextAlignment(.center)
-            })
+            }
         }
         
         init(_ card: EmojiMemoryGame.Card) {
@@ -95,8 +88,10 @@
         
         private struct CardDrawingConstants {
             static let cornerRadius: CGFloat = 10
-            static let lineWidth: CGFloat = 3
-            static let fontScale: CGFloat = 0.4
+            static let lineWidth: CGFloat = 2
+            static let fontScale: CGFloat = 0.5
+            static let piePadding: CGFloat = 5
+            static let pieOpacity: Double = 0.5
         }
         
         private func font(in size: CGSize) -> Font {
@@ -109,7 +104,8 @@
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             let game = EmojiMemoryGame()
-            Group {
+            game.choose(game.cards.first!)
+            return Group {
                 EmojiMemoryGameView(game)
                     .preferredColorScheme(.light)
                     .previewDisplayName("Light Mode")
