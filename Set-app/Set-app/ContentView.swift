0f38db1a9afedcfc2d8580099d843e55f16e7682
Allen Hammock <allenhammock42@gmail.com>
2021-09-12 20:10:39 -0700

    create empty iOS app called Set in preparation for assignment 3

---
+++ Set-app/Set-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set-app
+//
+//  Created by Allen Hammock on 9/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set-app/Set-app/Set.swift
@@ -0,0 +1,17 @@
+//
+//  Set.swift
+//  Set-app
+//
+//  Created by Allen Hammock on 9/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Set: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
